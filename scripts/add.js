afe89353a4c48bcbc3b8a76620a969b968498aff
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2024-02-29 22:48:21 -0300

    new: option to remove items

--- scripts/add.js
+++ scripts/add.js
@@ -1,13 +1,17 @@
 const addButton = document.getElementById("add-button");
+const removeAllButton = document.getElementById("remove-all-button");
 const grid = document.getElementById("grid");
+let lastId = 0;
 
 addButton.addEventListener("click", () => {
-  total = gridItems.length;
-
   const newGridItem = document.createElement("div");
-  newGridItem.setAttribute("id", `grid-snap-${total}`);
-  newGridItem.setAttribute("class", "snap");
-  newGridItem.innerHTML = `Drag me ${total}`;
+  newGridItem.setAttribute("id", `grid-item-${lastId}`);
+  newGridItem.setAttribute("class", "item");
+
+  const removeIcon = "<i class='bi bi-x'></i>";
+  const removeButton = `<button class="removeButton" onClick="removeItem(${lastId})">${removeIcon}</button>`;
+
+  newGridItem.innerHTML = `Drag me ${removeButton}`;
   grid.appendChild(newGridItem);
 
   gridItems.push({
@@ -15,8 +19,29 @@ addButton.addEventListener("click", () => {
     x: 0,
     y: 0,
     initiated: false,
+    id: lastId,
   });
   console.log(gridItems);
-  console.log(total);
+  console.log(lastId);
+  lastId++;
   startGrid();
+});
+
+const removeItem = (id) => {
+  let found = false;
+  for (let i = 0; !found; i++) {
+    console.log(gridItems[i].id, id);
+    if (gridItems[i].id === id) {
+      found = true;
+      item = gridItems[i];
+      grid.removeChild(item.item);
+      gridItems = gridItems.filter((e) => e.id !== id);
+    }
+  }
+  console.log(gridItems);
+};
+
+removeAllButton.addEventListener("click", () => {
+    gridItems = [];
+    grid.innerHTML = '';
 });
