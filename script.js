5a197eb080dc68e37c0d21cbd92f778c4a3a3b0a
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2024-02-29 21:09:48 -0300

    new: support to multiples items

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-var element = document.getElementById("grid-snap");
-var x = 0;
-var y = 0;
+const elements = [
+  { item: document.getElementById("grid-snap-1"), x: 0, y: 0 },
+  { item: document.getElementById("grid-snap-2"), x: 0, y: 0 },
+];
 
-interact(element)
-  .draggable({
-    modifiers: [
-      interact.modifiers.snap({
-        targets: [interact.snappers.grid({ x: 0, y: 0 })],
-        range: Infinity,
-        relativePoints: [{ x: 0, y: 0 }],
-      }),
-      interact.modifiers.restrict({
-        restriction: element.parentNode,
-        elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
-        endOnly: true,
-      }),
-    ],
-    inertia: true,
-  })
-  .on("dragmove", function (event) {
-    x += event.dx;
-    y += event.dy;
+elements.forEach((element) =>
+  interact(element.item)
+    .draggable({
+      modifiers: [
+        interact.modifiers.snap({
+          targets: [interact.snappers.grid({ x: 0, y: 0 })],
+          range: Infinity,
+          relativePoints: [{ x: 0, y: 0 }],
+        }),
+        interact.modifiers.restrict({
+          restriction: element.item.parentNode,
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+          endOnly: true,
+        }),
+      ],
+      inertia: true,
+    })
+    .on("dragmove", function (event) {
+      element.x += event.dx;
+      element.y += event.dy;
 
-    event.target.style.transform = "translate(" + x + "px, " + y + "px)";
-  });
+      event.target.style.transform = "translate(" + element.x + "px, " + element.y + "px)";
+    })
+);
